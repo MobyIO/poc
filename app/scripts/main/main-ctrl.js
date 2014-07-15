60481044f80bc9cad40ee3cc2c5a1ddec905c88d
Ubuntu <ubuntu@ip-10-144-205-49.ec2.internal>
2014-07-15 17:48:18 +0000

    Fix overflow issue on scroll

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -6,5 +6,20 @@ angular.module('moby')
            dimensions: [4,1], // specifies number of columns and rows
     };
     var nav_footer_image_size = [undefined,50];
-    $scope.navs = [{class: "footer_nav_home", size: nav_footer_image_size}, {class: "footer_nav_popular", size: nav_footer_image_size}, {class: "footer_nav_wish_list", size: nav_footer_image_size}, {class: "footer_nav_cart", size: nav_footer_image_size}];
+    $scope.navs = [{class: "footer_nav_home", size: nav_footer_image_size}, {class: "footer_nav_search", size: nav_footer_image_size}, {class: "footer_nav_popular", size: nav_footer_image_size}, {class: "footer_nav_cart", size: nav_footer_image_size}];
+
+
+
+    var EventHandler = $famous['famous/core/EventHandler'];
+    $scope.eventHandler = new EventHandler();
+
+    $scope.list = [{content: "famous"}, {content: "angular"}, {content: "rocks!"},{content: "famous"}, {content: "angular"}, {content: "rocks!"},{content: "famous"}, {content: "angular"}, {content: "rocks!"},{content: "famous"}, {content: "angular"}, {content: "rocks!"}];
+
+    $scope.options = {
+          myScrollView: {
+        paginated: false,
+        speedLimit: 5,
+        direction: 1,
+      }
+    };
 });
